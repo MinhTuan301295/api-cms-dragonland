9f80fff5ebb5c65a74f9128b46e385905e870c3c
TuanHM <cnpmvtvcab@gmail.com>
2025-07-17 11:23:42 +0700

    change code query

--- src/analytics/analytics.controller.ts
+++ src/analytics/analytics.controller.ts
@@ -1,16 +1,11 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 import { AnalyticsService } from './analytics.service';
 
 @Controller('api/analytics')
 export class AnalyticsController {
   constructor(private readonly analyticsService: AnalyticsService) {}
-
-  // @Get('summary')
-  // getSummary(@Query('from') from?: string, @Query('to') to?: string) {
-  //   return this.analyticsService.getSummary(from, to);
-  // }
   @Get('summary')
-  getSummary() {
-    return { message: 'Hello from API' }; // ✅ Tạm thời hardcode test
+  getSummary(@Query('from') from?: string, @Query('to') to?: string) {
+    return this.analyticsService.getSummary(from, to);
   }
 }
