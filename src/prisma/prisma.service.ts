a53cfb1856f589ce9880f081e57cd08e2b4535c6
TuanHM <cnpmvtvcab@gmail.com>
2025-07-17 10:37:31 +0700

    add log build

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,3 +1,20 @@
+// import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
+// import { PrismaClient } from '@prisma/client';
+
+// @Injectable()
+// export class PrismaService
+//   extends PrismaClient
+//   implements OnModuleInit, OnModuleDestroy
+// {
+//   async onModuleInit() {
+//     await this.$connect();
+//   }
+
+//   async onModuleDestroy() {
+//     await this.$disconnect();
+//   }
+// }
+
 import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
@@ -7,6 +24,12 @@ export class PrismaService
   implements OnModuleInit, OnModuleDestroy
 {
   async onModuleInit() {
+    if (!process.env.DATABASE_URL) {
+      console.error('❌ DATABASE_URL is not defined in environment variables!');
+    } else {
+      console.log('✅ DATABASE_URL is loaded.');
+    }
+
     await this.$connect();
   }
 
