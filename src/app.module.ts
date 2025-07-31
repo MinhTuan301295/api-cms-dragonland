f6e4f72e6a0922ff761b4859eab838f4a13b5a16
TuanHM <cnpmvtvcab@gmail.com>
2025-08-01 00:23:19 +0700

    check health

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AnalyticsModule } from './analytics/analytics.module';
-
+import { AppController } from './app.controller';
+import { PrismaModule } from './prisma/prisma.module';
 @Module({
-  imports: [AnalyticsModule],
+  imports: [AnalyticsModule, PrismaModule],
+  controllers: [AppController],
 })
 export class AppModule {}
