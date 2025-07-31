14a96f86d1d9f6df5ff393ade6925153558d851e
TuanHM <cnpmvtvcab@gmail.com>
2025-07-31 23:41:31 +0700

    update port

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,3 @@
-// import { NestFactory } from '@nestjs/core';
-// import { AppModule } from './app.module';
-
-// async function bootstrap() {
-//   const app = await NestFactory.create(AppModule);
-//   app.enableCors();
-//   const port = process.env.PORT; // ❗️KHÔNG có mặc định
-//   if (!port) {
-//     throw new Error('❌ Missing PORT environment variable');
-//   }
-//   await app.listen(port);
-// }
-// bootstrap();
-
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
@@ -19,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors(); // ✅ quan trọng nếu bạn gọi từ trình duyệt
   const port = process.env.PORT || 3000;
-  await app.listen(port);
+  await app.listen(port, '0.0.0.0'); // ✅ quan trọng: bind public interface
   console.log(`🚀 App is running on http://localhost:${port}`);
 }
 bootstrap();
