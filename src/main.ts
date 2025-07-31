553006bdf8a099df66aca9f8de27c8be60c6d2fc
TuanHM <cnpmvtvcab@gmail.com>
2025-08-01 00:41:59 +0700

    check health and add app serviceq

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors(); // ✅ quan trọng nếu bạn gọi từ trình duyệt
   const port = process.env.PORT || 3000;
-  await app.listen(port, '0.0.0.0'); // ✅ quan trọng: bind public interface
+  await app.listen(process.env.PORT || 8080, '0.0.0.0');
   console.log(`🚀 App is running on http://localhost:${port}`);
 }
 bootstrap();
