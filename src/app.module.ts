a5f6f35b018650df789a26d53997906bdec437a8
TuanHM <cnpmvtvcab@gmail.com>
2025-08-01 00:33:16 +0700

    check health and add app service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AnalyticsModule } from './analytics/analytics.module';
 import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
 @Module({
   imports: [AnalyticsModule],
   controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
