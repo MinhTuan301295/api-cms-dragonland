300230367a4a8a8e7a3b8e4eac25687095e98f36
TuanHM <cnpmvtvcab@gmail.com>
2025-08-01 11:09:34 +0700

    update main

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors(); // ✅ quan trọng nếu bạn gọi từ trình duyệt
-  const port = process.env.PORT || 3000;
-  await app.listen(process.env.PORT || 8080, '0.0.0.0');
+  app.enableCors();
+  const port = process.env.PORT || 8080;
+  await app.listen(port, '0.0.0.0');
   console.log(`🚀 App is running on http://localhost:${port}`);
 }
 bootstrap();
